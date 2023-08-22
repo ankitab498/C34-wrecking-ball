8a57efd21c001211ff2beff1f409468798c5c370
ankitab498 <75402577+ankitab498@users.noreply.github.com>
2023-08-22 16:07:55 +0530

    Update Box.js

--- wrecking ball/Box.js
+++ wrecking ball/Box.js
@@ -5,11 +5,7 @@ class Box {
         'friction':1.0,
         'density':0.04,
     }
-    this.body = Bodies.rectangle(x, y, width, height, options);
-    this.width = width;
-    this.height = height;
-    
-    World.add(world, this.body);
+  
   }
   display(){
     var pos =this.body.position;
